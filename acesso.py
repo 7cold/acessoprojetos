c24c122f6112278b21471ad59e58c665a3dfc480
Leonardo Coldibelli <59493948+7cold@users.noreply.github.com>
2025-05-15 19:37:44 -0300

    Update acesso.py

--- acesso.py
+++ acesso.py
@@ -4,17 +4,19 @@ from time import sleep
 
 options = Options()
 options.add_argument("--headless")
+options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-options.binary_location = "/usr/bin/chromium-browser"
+options.add_argument("--remote-debugging-port=9222")
 
+# Caminho do driver padrão no GitHub Actions
 driver = webdriver.Chrome(options=options)
 
-url = "http://www.7cold.com/glauco"  # <- Substitua pelo seu site real
+url = "https://www.7cold.com/glauco"  # Substitua aqui
 print(f"Acessando: {url}")
 driver.get(url)
 
-sleep(10)  # Espera 10 segundos
+sleep(10)
 
 print("Fechando navegador")
 driver.quit()
